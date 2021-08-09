f8ff1f49892c7a713a8d67e8595c8bfe8b8ca51a
TahiJameel <tahajameel.co@gmail.com>
2021-08-09 17:05:39 +0500

    set form to submitting and log all username and password

--- server/public/index.js
+++ server/public/index.js
@@ -5,4 +5,11 @@ import './desktop.js';
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.getElementById('app');
   app.innerHTML = '<install-page></install-page>';
+  const install_form = document.getElementById('install_form');
+  install_form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const userpassword = document.getElementById('userpassword').value;
+    console.log(`UserName: ${username} Password: ${userpassword}`);
+  });
 });

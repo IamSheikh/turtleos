421e908a411215fbc4175bf3f3388e2d49a71397
TahiJameel <tahajameel.co@gmail.com>
2021-08-09 16:47:18 +0500

    Basic JavaScript Setup

--- server/public/index.js
+++ server/public/index.js
@@ -1,1 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {});
+import './installPage.js';
+import './loginPage.js';
+import './desktop.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = document.getElementById('app');
+  app.innerHTML = '<install-page></install-page>';
+});

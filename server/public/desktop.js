d7c53e38455c6780aeab7e76724faebc2b16052d
TahiJameel <tahajameel.co@gmail.com>
2021-08-11 12:30:49 +0500

    desktop setup

--- server/public/desktop.js
+++ server/public/desktop.js
@@ -1,7 +1,12 @@
 class DesktopPage extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-        <h1>Desktop</h1>
+        <div id="apps">
+          <div id="app-1" class="apps">
+            <i class="fa fa-terminal icon"></i>
+            <h3 class="app-text">Terminal</h3>
+          </div>
+        </div>
       `;
   }
 }

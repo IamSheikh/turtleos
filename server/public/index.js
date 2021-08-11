7089109c6f571acedd9fa093a53be0c1d79cbdd4
TahiJameel <tahajameel.co@gmail.com>
2021-08-11 13:15:15 +0500

    fix error and make first command pwd

--- server/public/index.js
+++ server/public/index.js
@@ -8,18 +8,11 @@ function speak(textForSpeak) {
   synth.speak(textToSpeak);
 }
 
-function openTerminalBtn() {
-  const openTerminalBtn = document.createElement('button');
-  openTerminalBtn.className = 'openTerminalBtn';
-  openTerminalBtn.style.backgroundColor = 'red';
-  openTerminalBtn.style.color = 'white';
-  openTerminalBtn.style.padding = '1rem';
-  openTerminalBtn.style.border = 'none';
-  openTerminalBtn.style.marginLeft = '0.5rem';
-  openTerminalBtn.style.marginRight = '0.5rem';
-  openTerminalBtn.style.cursor = 'pointer';
-  openTerminalBtn.textContent = 'Open Terminal';
-  document.body.append(openTerminalBtn);
+function Response(response, element) {
+  this.response = response;
+  this.element = element;
+  this.element.style.margin = '1rem';
+  this.element.append(response);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -39,6 +32,43 @@ document.addEventListener('DOMContentLoaded', () => {
       if (loginPassword.value == window.userpassword) {
         speak(`Welcome ${window.name}`);
         app.innerHTML = '<desktop-page></desktop-page>';
+        const app1 = document.querySelector('desktop-page #apps #app-1');
+        console.log(app1);
+        app1.addEventListener('click', () => {
+          openTerminal();
+        });
+        function openTerminal() {
+          let cli = window.prompt(
+            `${window.username}${window.name.split(' ').join('')}-TurtleOS$`
+          );
+          const responseDiv = document.createElement('div');
+          responseDiv.style.fontSize = '1rem';
+          document.body.append(responseDiv);
+          switch (cli) {
+            case 'pwd':
+              document.querySelector('desktop-page').style.display = 'none';
+              new Response('Hello', responseDiv);
+              openTerminalBtn();
+              break;
+          }
+          console.log(cli);
+          function openTerminalBtn() {
+            const openTerminalBtn = document.createElement('button');
+            openTerminalBtn.className = 'openTerminalBtn';
+            openTerminalBtn.style.backgroundColor = 'red';
+            openTerminalBtn.style.color = 'white';
+            openTerminalBtn.style.padding = '1rem';
+            openTerminalBtn.style.border = 'none';
+            openTerminalBtn.style.marginLeft = '0.5rem';
+            openTerminalBtn.style.marginRight = '0.5rem';
+            openTerminalBtn.style.cursor = 'pointer';
+            openTerminalBtn.textContent = 'Open Terminal';
+            openTerminalBtn.addEventListener('click', () => {
+              openTerminal();
+            });
+            document.body.append(openTerminalBtn);
+          }
+        }
         console.log('Successfully Logged In');
       } else {
         speak('Please enter correct password');
